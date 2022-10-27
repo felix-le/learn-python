fb82ad479b2cd65fe8ea3c7cb5c24af685af4bdc
Viet Anh Le <mc.vietanh@gmail.com>
2022-10-27 09:03:36 -0400

    learn bytes

--- lynda/lists.py
+++ lynda/lists.py
@@ -1,21 +1,25 @@
-myList = [1,2,3,4,5,6,7,8,9,10]
-# print myList from index 3 to end
-# print(myList[3:])
-# print myList from index 0 to 6 with step size is 2
-# print(myList[0:6:2])
-# A different way
-# print(myList[::2])
+# myList = [1,2,3,4,5,6,7,8,9,10]
+# # print myList from index 3 to end
+# # print(myList[3:])
+# # print myList from index 0 to 6 with step size is 2
+# # print(myList[0:6:2])
+# # A different way
+# # print(myList[::2])
 
-# New list from 0 to 100
+# # New list from 0 to 100
 
-# myNewList = list(range(100))
-# print(myNewList)
-# print(myNewList[::10])
-# print(myNewList[::-10]) 
+# # myNewList = list(range(100))
+# # print(myNewList)
+# # print(myNewList[::10])
+# # print(myNewList[::-10]) 
 
-myList.append(11) 
-print(myList) #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+# myList.append(11) 
+# print(myList) #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-myList.insert(0, 'a new value')# ['a new value', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+# myList.insert(0, 'a new value')# ['a new value', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-myList.remove('a new value') # [ 1, 2, 3, 4, 6, 7, 8, 9, 10, 11]
+# myList.remove('a new value') # [ 1, 2, 3, 4, 6, 7, 8, 9, 10, 11]
+
+# x = myList.pop() # [ 1, 2, 3, 4, 6, 7, 8, 9, 10]
+# x # 11
+# myList # [ 1, 2, 3, 4, 6, 7, 8, 9, 10]
--- lynda/note.md
+++ lynda/note.md
@@ -1,3 +1,44 @@
+# Data types
+
+```python
+# Boolean
+a = True
+b = False
+
+print(a and b) # False
+print(a or b) # True
+print(not a) # False
+print(not b) # True
+print(b or a) # True
+print(b and a) # False
+print(a and not b) # True
+print(not(a or b)) # False
+```
+
+## Strings
+
+```python
+myString = 'my name is felix le, hello everyone'
+print(myString[0:8]) # my name
+print(myString[0:8:2]) # m ae
+print(myString[6:]) # is felix le, hello everyone
+print('n' in myString) # True
+print('hello' in myString) # True
+print(len(myString)) # 31
+print(f'Length of myString is {len(myString)}') # Length of myString is 31
+# new lines
+myNewString= '''This is a new string
+we can use this to write a long string
+and it will be printed in multiple lines
+. it will stop when we use 3 single quotes
+
+'''
+print(myNewString)
+
+
+
+```
+
 # Lists
 
 ```python
